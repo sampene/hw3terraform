02edc739aa1a9a4f92e0b5788979410bf9a2805f
Yaw Sampene Buadu <ysbuadu@gmail.com>
2020-12-26 19:51:39 +0000

    fixed config error with quotes

--- main.tf
+++ main.tf
@@ -38,7 +38,7 @@ resource "aws_security_group" "asg_ec2_example" {
     from_port = var.server_port
     to_port = var.server_port
     protocol = "tcp"
-    cidr_blocks = [0.0.0.0/0]
+    cidr_blocks = ["0.0.0.0/0"]
     
 }
 }
